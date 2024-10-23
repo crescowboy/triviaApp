4da814c844e2a470279172b75ad1bbe6bd0cb96d
wcrespo <wellingtoncrespo2001@gmail.com>
2024-10-22 22:14:53 -0500

    MovieHeader

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -4,16 +4,25 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import {RootStackParams} from '../../navigation/Navigation';
 import {useMovie} from '../../hooks/useMovie';
+import {MovieHeader} from '../../components/movie/MovieHeader';
 
 type Props = StackScreenProps<RootStackParams, 'Details'>;
 
 export const DetailsScreen = ({route}: Props) => {
   const {movieId} = route.params;
-  const {} = useMovie(movieId);
+  const {isLoading, movie} = useMovie(movieId);
+
+  if (isLoading) {
+    return <Text>Loading</Text>;
+  }
 
   return (
     <View>
-      <Text>DetailsScreen</Text>
+      <MovieHeader
+        originalTitle={movie!.originalTitle}
+        title={movie!.title}
+        poster={movie!.poster}
+      />
     </View>
   );
 };
