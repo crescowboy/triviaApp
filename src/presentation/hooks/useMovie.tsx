0672cd3c4da327708546a77f8eba6990ff06ee19
wcrespo <wellingtoncrespo2001@gmail.com>
2024-10-31 23:46:19 -0500

    add cast

--- src/presentation/hooks/useMovie.tsx
+++ src/presentation/hooks/useMovie.tsx
@@ -2,10 +2,12 @@ import React, {useEffect, useState} from 'react';
 import * as UseCases from '../../core/use-cases';
 import {FullMovie} from '../../core/entities/movie.entity';
 import {movieDBFetcher} from '../../config/adapters/movieDB.adapter';
+import {Cast} from '../../core/entities/cast.entity';
 
 export const useMovie = (movieId: number) => {
   const [isLoading, setIsLoading] = useState(true);
   const [movie, setMovie] = useState<FullMovie>();
+  const [cast, setCast] = useState<Cast>();
 
   useEffect(() => {
     loadMovie();
@@ -13,17 +15,26 @@ export const useMovie = (movieId: number) => {
 
   const loadMovie = async () => {
     setIsLoading(true);
-    const fullMovie = await UseCases.getMovieByIdUseCase(
+    const fullMoviePromise = UseCases.getMovieByIdUseCase(
       movieDBFetcher,
       movieId
     );
+    const castPromise = UseCases.getMovieCastUsecase(movieDBFetcher, movieId);
+
+    const [fullMovie, cast] = await Promise.all([
+      fullMoviePromise,
+      castPromise
+    ]);
+
     setMovie(fullMovie);
+    setCast(cast);
     setIsLoading(false);
-    console.log(fullMovie);
+    console.log({cast});
   };
 
   return {
     isLoading,
-    movie
+    movie,
+    cast
   };
 };
--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -5,24 +5,30 @@ import {Text, View} from 'react-native';
 import {RootStackParams} from '../../navigation/Navigation';
 import {useMovie} from '../../hooks/useMovie';
 import {MovieHeader} from '../../components/movie/MovieHeader';
+import {MovieDetails} from '../../components/movie/MovieDetails';
+import {ScrollView} from 'react-native-gesture-handler';
 
 type Props = StackScreenProps<RootStackParams, 'Details'>;
 
 export const DetailsScreen = ({route}: Props) => {
   const {movieId} = route.params;
-  const {isLoading, movie} = useMovie(movieId);
+  const {isLoading, movie, cast = []} = useMovie(movieId);
 
   if (isLoading) {
     return <Text>Loading</Text>;
   }
 
   return (
-    <View>
-      <MovieHeader
-        originalTitle={movie!.originalTitle}
-        title={movie!.title}
-        poster={movie!.poster}
-      />
-    </View>
+    <ScrollView>
+      <View>
+        <MovieHeader
+          originalTitle={movie!.originalTitle}
+          title={movie!.title}
+          poster={movie!.poster}
+        />
+
+        <MovieDetails movie={movie!} cast={cast} />
+      </View>
+    </ScrollView>
   );
 };
